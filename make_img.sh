3d2e0fb50d43b95334f2e718b276859e091085ba
Edvin Alvarado <ealvarado301@gmail.com>
2026-08-06 15:13:58 -0500

    changed path from usb-to-archive_deb to usb-to-archive

---
+++ make_img.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+if [ -f "usb-to-nas.conf" ]; then
+	source usb-to-nas.conf
+elif [ -f "/opt/usb-to-nas/usb-to-nas.conf" ]; then
+	source /opt/usb-to-nas/usb-to-nas.conf
+fi
+
+IMG_FILE="${IMG_FILE:-/mnt/storage/drive.img}"
+STORAGE_SIZE="${STORAGE_SIZE:-16G}"
+
+
+## Script
+sudo truncate -s $STORAGE_SIZE $IMG_FILE
+sudo mkfs.fat -F 32 $IMG_FILE
